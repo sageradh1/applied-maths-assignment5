fe3d6338c2b7ec277349dea456e6ffb9d5ce64ec
Sagar Adhikari <sageradh@gmail.com>
2024-01-29 01:04:52 -0500

    Added Multiclass functions

--- multiclass_model.py
+++ multiclass_model.py
@@ -9,6 +9,7 @@ def define_dense_model_single_layer(input_length, activation_f='sigmoid', output
     activation_f: the activation function
     output_length: the number of outputs (number of neurons)"""
     model = keras.Sequential()
+    model.add(keras.layers.Dense(units=output_length, input_dim=input_length, activation=activation_f))
     return model
 
 def define_dense_model_with_hidden_layer(input_length, 
@@ -20,11 +21,10 @@ def define_dense_model_with_hidden_layer(input_length,
     activation_func_array: the activation function for the hidden layer and the output layer
     hidden_layer_size: the number of neurons in the hidden layer
     output_length: the number of outputs (number of neurons in the output layer)"""
-
     model = keras.Sequential()
+    model.add(keras.layers.Dense(units=hidden_layer_size, input_dim=input_length, activation=activation_func_array[0]))
+    model.add(keras.layers.Dense(units=output_length, activation=activation_func_array[1]))
     return model
-
-
 
 def get_mnist_data():
     """Get the MNIST data."""
@@ -40,14 +40,20 @@ def fit_mnist_model(x_train, y_train, model, epochs=2, batch_size=2):
 
     then fit the model on the training data. (pass the epochs and batch_size params)
     """
-    # model.compile  ... 
-    # model.fit ...    
+    optimizer = keras.optimizers.Adam(learning_rate=0.001)
+    loss_function = 'categorical_crossentropy'
+    metrics_list = ['accuracy']
+
+    model.compile(optimizer=optimizer, loss=loss_function, metrics=metrics_list)
+    y_train = keras.utils.to_categorical(y_train, num_classes=10)
+    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)   
     return model
   
 def evaluate_mnist_model(x_test, y_test, model):
     """Evaluate the model on the test data.
     Hint: use model.evaluate() to evaluate the model on the test data.
     """
-    loss, accuracy = None, None # model evaluate
+    y_test = keras.utils.to_categorical(y_test, num_classes=10)
+    loss, accuracy = model.evaluate(x_test, y_test)
     return loss, accuracy
 
